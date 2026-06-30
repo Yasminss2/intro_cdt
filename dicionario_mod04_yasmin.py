484aa27281f7efc8ac1b25b8f5843dade9ad4d47
Aluna Vocação - Yasmin Santos Sousa <yasminssousousa@gmail.com>
2026-06-30 17:11:43 -0300

    Add files via upload

---
+++ dicionario_mod04_yasmin.py
@@ -0,0 +1,26 @@
+print('modulo04 - estrutura de dados - dicionario \n')
+print('-' * 40 )
+
+'''
+Cardápio_semana = {
+   "Segundas-feira": "Salada de maionese",
+   "Terça-feira": "Arroz e farofa",
+   "Quarta-feira": "Dia da Pizza",
+   "Quinta-feira": "Strogonof",
+   "Sexta-feira": "Sorvete ou Ácai",
+   "Sabado": "Churrasco",
+   "Domingo": "Salada de frutas",
+}
+
+nome_usuario = imput ('Digite um dia da semana: ')
+
+dia_escolhido = imput ("Digite um dia da semana para saber o menu ").
+
+if dia_escolhido in cardapio_semana:
+Comida = cardaapio_semana(dia_escolhido)
+
+print (f"No (dia_escolhido), o dia é de comer: {comida}!")
+
+else:
+    print("Desculpe, nos nao temos esse dia no menu")
+
